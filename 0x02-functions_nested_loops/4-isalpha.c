7ae3012128811858a59e5565a1227788fc977f73
ignisious254 <ignisioussmiles@gmail.com>
2023-07-18 04:20:37 -0700

    Check for alphabetic letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - check for the letters of the alphabet
+ * Return: 1 if letters or 0 for anything else
+ */
+
+int _isalpha(int c)
+{
+if ((c>=65 && c<=97) || (c>=97 && c<=122))
+{
+return (1);	
+}
+return (0);
+}
