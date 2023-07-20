b8b3c30afc2a845ca9a34565b1c202a73323af77
ignisious254 <ignisioussmiles@gmail.com>
2023-07-20 03:23:51 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,19 +2,17 @@
 
 /**
  * print_numbers - print 0-9
- * Return: void
+ * return: void
  */
 
 void print_numbers(void)
-
 {
+	char c;
 
-	char i;
-
-	for (i = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
-
+		_putchar(c);
 	}
 	_putchar('\n');
 }
+
