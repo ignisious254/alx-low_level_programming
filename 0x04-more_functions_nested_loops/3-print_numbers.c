e7a5dcdb10af5d85a108c6c9926c4638d03a5430
ignisious254 <ignisioussmiles@gmail.com>
2023-07-20 02:50:05 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0-9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+int i;
+
+i = 0;
+
+while(i < 10)
+{
+_putchar(i + '0');
+i++;
+}
+{
+_putchar("\n");
+}
+return (0);
+}
