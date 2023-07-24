d5be9cd6ce60f43ffbff165f64a375a7db84b6e4
ignisious254 <ignisioussmiles@gmail.com>
2023-07-24 13:36:09 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,14 +5,15 @@
  * Return: nothing
  */
 void puts2(char *str)
-{       int i;
-	
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                if (i % 2 == 0)
-                {
-                        _putchar(i + '0');
-                }
-        }
-        _putchar('\n');
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
