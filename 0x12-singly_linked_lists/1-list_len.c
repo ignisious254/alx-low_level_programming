2382db662d5f0b79dff9e96e586370d26ecaec2a
ignisious254 <ignisioussmile@gmail.com>
2023-08-24 05:44:29 -0700

    Task 1 returning the number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * list_len -  print the number of elements in a linked list
+ * @h: pointer to the list
+ *
+ * Return: the number of elemnts in the list
+ */
+
 size_t list_len(const list_t *h)
 {
+	size_t nodes = 0;
 
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
 }
