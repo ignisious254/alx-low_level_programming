6767bce3fbf6e88a369f2f37cb94d39c57b817bc
ignisious254 <ignisioussmile@gmail.com>
2023-08-24 07:35:59 -0700

    add new node at beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,10 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;/*declare new node*/
+	unsigned int len = 0;
+
+	while(str[len])
+		len++;
 
 	if (str == NULL)/*string cant be empty*/
 		return (NULL);
@@ -23,6 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);/*duplicate the string with strdup*/
+	new->len = len;
 	new_node->next = *head;/*change position of the head to the new node*/
 	*head = new_node;
 
